405bcc0a06c11cef8d15792a9a166d672332502a
Naitik <shahnaitik2000@gmail.com>
2022-05-02 10:37:34 +0530

    Trying Google Drive API

--- script.js
+++ script.js
@@ -41,7 +41,10 @@ function loadLabeledImages() {
     labels.map(async label => {
       const descriptions = []
       for (let i = 1; i <= 2; i++) {
-        const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/naitik2314/Face-Recognition-JavaScript/master/labeled_images/${label}/${i}.jpg`)
+        //const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/naitik2314/Face-Recognition-JavaScript/master/labeled_images/${label}/${i}.jpg`)
+        //Drive folder on this
+        const img = await faceapi.fetchImage(`https://drive.google.com/drive/folders/1j4-VTfaUbz-2unrV8acQJH_7YgiLEPVD?usp=sharing/${label}/${1}.jpg`)
+        //const img = await faceapi.fetchImage(`labeled_images/${label}/${i}.jpg`)
         const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
         descriptions.push(detections.descriptor)
       }
